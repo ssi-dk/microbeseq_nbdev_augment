4dc85cf5672f7ddfe2a74ae149410dd9f0f8d7a9
Kim Ng <kimleeng@gmail.com>
2024-03-08 14:20:22 +0100

    sed -i works differently on mac and linux, changed it to perl

--- make_nbdev_env.sh
+++ make_nbdev_env.sh
@@ -10,6 +10,13 @@ then
     exit 1
 fi
 
+# Check for perl since we use perl for sed
+if ! command -v perl &> /dev/null
+then
+    echo "perl could not be found, please install perl before running this script"
+    exit 1
+fi
+
 # Check that the current folder is empty
 if [ "$(ls -A .)" ]; then
     echo "Current directory is not empty, please run this in a blank directory"
@@ -109,16 +116,14 @@ echo "    core_hello_world=$GIT_REPO_NAME.core:cli" >> settings.ini;
 echo "    hello_two_world=$GIT_REPO_NAME.hello_world:cli" >> settings.ini;
 
 # replace the marker in the file $NBDEV_PROJECT_FOLDER/nbs/00_core.ipynb, it can occur multiple times
-sed -i '' "s/\$PACKAGE_NAME/$GIT_REPO_NAME/g" $NBDEV_PROJECT_FOLDER/nbs/00_core.ipynb;
-#print the command which was run
-
-sed -i '' "s/\$PACKAGE_NAME/$GIT_REPO_NAME/g" $NBDEV_PROJECT_FOLDER/nbs/01_hello_world.ipynb;
+perl -pi -e "s/\$PACKAGE_NAME/$GIT_REPO_NAME/g" "$NBDEV_PROJECT_FOLDER/nbs/00_core.ipynb"
+perl -pi -e "s/\$PACKAGE_NAME/$GIT_REPO_NAME/g" "$NBDEV_PROJECT_FOLDER/nbs/01_hello_world.ipynb"
 
 # make the value of GIT_REPO_NAME to all caps
 GIT_REPO_NAME_UPPER=$(echo $GIT_REPO_NAME | tr '[:lower:]' '[:upper:]');
 # for the config.default.env, adjust project name to the GIT_REPO_NAME.
-sed -i '' "s/PROJECTNAME/$GIT_REPO_NAME_UPPER/g" $NBDEV_PROJECT_FOLDER/$GIT_REPO_NAME/config/config.default.env;
-sed -i '' "s/PROJECTNAME/$GIT_REPO_NAME_UPPER/g" $NBDEV_PROJECT_FOLDER/$GIT_REPO_NAME/config/config.default.yaml;
+perl -pi -e "s/PROJECTNAME/$GIT_REPO_NAME_UPPER/g" $NBDEV_PROJECT_FOLDER/$GIT_REPO_NAME/config/config.default.env;
+perl -pi -e "s/PROJECTNAME/$GIT_REPO_NAME_UPPER/g" $NBDEV_PROJECT_FOLDER/$GIT_REPO_NAME/config/config.default.yaml;
 
 echo "include config/config.default.env" >> MANIFEST.in;
 echo "include config/config.default.yaml" >> MANIFEST.in;
